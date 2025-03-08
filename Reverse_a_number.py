896156ea2d3658a0c5032d4a633d73ead44467a9
Sushyam Nagallapati <sushyamraju@gmail.com>
2025-03-08 13:46:46 -0500

    Reverse_a_number

--- Reverse_a_number.py
+++ Reverse_a_number.py
@@ -2,6 +2,9 @@ number = int(input("Enter a number: "))
 reverse_number = 0
 
 while number > 0:
-    reverse_number = reverse_number * 10 + (number % 10)
+    
+    remainder = number % 10
+    reverse_number = reverse_number * 10 + remainder
     number //= 10    
+    
 print(reverse_number)
