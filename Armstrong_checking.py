0f84dba8280265cba887272d68a1228d2e3d85d2
Sushyam Nagallapati <sushyamraju@gmail.com>
2025-03-11 17:49:21 -0400

    Armstrong_checking

--- Armstrong_checking.py
+++ Armstrong_checking.py
@@ -17,19 +17,20 @@
 
 # Or
 
+
 number = int(input("Enter a number: "))
-
 original_number = number
-reverse_number = 0
 
-digits = len(str(number))
+sum_of_powers = 0
+
+num_digits = len(str(number))
 
 while number > 0:
-    remainder = number % 10
-    reverse_number += remainder ** digits
+    digit = number % 10
+    sum_of_powers += digit ** num_digits
     number //= 10
     
-if original_number == reverse_number:
-    print("It is an Armstrong number")
+if sum_of_powers == original_number:
+    print(f"{original_number} is an Armstrong number")
 else:
-    print("It is not an Armstrong number")
+    print(f"{original_number} is not an Armstrong number")
