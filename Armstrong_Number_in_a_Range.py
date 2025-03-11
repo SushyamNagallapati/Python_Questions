a33c814adfd84590c504203892347d8fdfc63e5f
Sushyam Nagallapati <sushyamraju@gmail.com>
2025-03-11 17:49:47 -0400

    Armstrong_Number_in_a_Range

--- Armstrong_Number_in_a_Range.py
+++ Armstrong_Number_in_a_Range.py
@@ -1,16 +1,20 @@
-number = int(input("Enter a number: "))
-original_number = number
+start, end = map(int, input("Enter Two Numbers: ").split())
 
-sum_of_powers = 0
+armstrong_numbers = []
 
-num_digits = len(str(number))
-
-while number > 0:
-    digit = number % 10
-    sum_of_powers += digit ** num_digits
-    number //= 10
+for num in range(start, end + 1):
+    original_number = num
     
-if sum_of_powers == original_number:
-    print(f"{original_number} is an Armstrong number")
-else:
-    print(f"{original_number} is not an Armstrong number")
+    sum_of_powers = 0
+
+    num_digits = len(str(num))
+    
+    while num > 0:
+        digit = num % 10
+        sum_of_powers += digit ** num_digits
+        num //= 10
+
+    if sum_of_powers == original_number:
+        armstrong_numbers.append(original_number)
+        
+print(f"Armstrog numbers between {start} and {end} are {armstrong_numbers}")
