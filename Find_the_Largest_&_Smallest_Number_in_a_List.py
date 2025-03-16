783c9d74ac41d0659ee0fc5220f0f5be19a0b381
Sushyam Nagallapati <sushyamraju@gmail.com>
2025-03-15 21:13:18 -0400

    Find_the_Largest_&_Smallest_Number_in_a_List

---
+++ Find_the_Largest_&_Smallest_Number_in_a_List.py
@@ -0,0 +1,7 @@
+numbers = list(map(int, input("Enter number with spaces: ").split()))
+
+smallest_numbers = min(numbers)
+largest_numbers = max(numbers)
+
+print(f"The smallest number in the list is: {smallest_numbers}")
+print(f"The largest number in the list is: {largest_numbers}")
