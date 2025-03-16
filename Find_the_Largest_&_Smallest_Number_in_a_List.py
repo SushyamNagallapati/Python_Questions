5be72fa9061903246c116dcef67963316e87a9cb
Sushyam Nagallapati <sushyamraju@gmail.com>
2025-03-15 21:20:43 -0400

    Find_the_Largest_&_Smallest_Number_in_a_List

--- Find_the_Largest_&_Smallest_Number_in_a_List.py
+++ Find_the_Largest_&_Smallest_Number_in_a_List.py
@@ -1,7 +1,25 @@
+# numbers = list(map(int, input("Enter number with spaces: ").split()))
+
+# smallest_numbers = min(numbers)
+# largest_numbers = max(numbers)
+
+# print(f"The smallest number in the list is: {smallest_numbers}")
+# print(f"The largest number in the list is: {largest_numbers}")
+
+
+# # OR
+
+
 numbers = list(map(int, input("Enter number with spaces: ").split()))
 
-smallest_numbers = min(numbers)
-largest_numbers = max(numbers)
+smallest_numbers = numbers[0]
+largest_numbers = numbers[0]
 
+for num in numbers: 
+    if num < smallest_numbers:
+        smallest_numbers = num
+    elif num > largest_numbers:
+        largest_numbers = num
+        
 print(f"The smallest number in the list is: {smallest_numbers}")
 print(f"The largest number in the list is: {largest_numbers}")
