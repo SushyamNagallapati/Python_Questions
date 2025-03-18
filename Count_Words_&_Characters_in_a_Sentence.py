be2c34c41cbf7eddcac50afb21e6a3db23816069
Sushyam Nagallapati <sushyamraju@gmail.com>
2025-03-18 15:59:08 -0400

    Count_Words_&_Characters_in_a_Sentence

---
+++ Count_Words_&_Characters_in_a_Sentence.py
@@ -0,0 +1,8 @@
+sentence = input("Enter sentence: ").lower()
+
+characters = len(sentence)
+
+words = len(sentence.split())
+
+print(f"The number of characters in the given sentence is: {characters}")
+print(f"The number of words in the given sentence is: {words}")
