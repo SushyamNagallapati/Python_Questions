417a183196e69c67acec369df46d2e69d9de353c
Sushyam Nagallapati <sushyamraju@gmail.com>
2025-03-18 16:05:18 -0400

    Count_Words_&_Characters_in_a_Sentence

--- Count_Words_&_Characters_in_a_Sentence.py
+++ Count_Words_&_Characters_in_a_Sentence.py
@@ -1,8 +1,24 @@
+# sentence = input("Enter sentence: ").lower()
+
+# characters = len(sentence)
+
+# words = len(sentence.split())
+
+# print(f"The number of characters in the given sentence is: {characters}")
+# print(f"The number of words in the given sentence is: {words}")
+
+
+# # OR
+
+
 sentence = input("Enter sentence: ").lower()
 
-characters = len(sentence)
+characters_with_spaces = len(sentence)
+
+characters_without_spaces = len(sentence.replace(" ", ""))
 
 words = len(sentence.split())
 
-print(f"The number of characters in the given sentence is: {characters}")
+print(f"The number of characters (with spaces) in the given sentence is: {characters_with_spaces}")
+print(f"The number of characters (without spaces) in the given sentence is: {characters_without_spaces}")
 print(f"The number of words in the given sentence is: {words}")
