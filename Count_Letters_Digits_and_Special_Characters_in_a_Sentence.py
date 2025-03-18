9c7bb6334e0afeb1c9a55c6d6b8d1afe2d1c9e5e
Sushyam Nagallapati <sushyamraju@gmail.com>
2025-03-18 16:16:40 -0400

    Count_Letters_Digits_and_Special_Characters_in_a_Sentence

---
+++ Count_Letters_Digits_and_Special_Characters_in_a_Sentence.py
@@ -0,0 +1,17 @@
+user = input("Enter a sentence: ").lower()
+
+letters = 0
+digits = 0
+special_characters = 0
+
+for i in user:
+    if i.isalpha():
+        letters += 1
+    elif i.isdigit():
+        digits += 1
+    else:
+        special_characters += 1
+        
+print(f"The number of Letters present in the given sentence is: {letters}")
+print(f"The number of Digits present in the given sentence is: {digits}")
+print(f"The number of Special Characters present in the given sentence is: {special_characters}")
