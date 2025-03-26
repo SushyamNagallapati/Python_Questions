5eff7eb3b34c3fb5089cb6ae6557f39113bbabbc
Sushyam Nagallapati <sushyamraju@gmail.com>
2025-03-26 15:50:53 -0400

    Number Pyramid in Reverse

--- Number Pyramid in Reverse.py
+++ Number Pyramid in Reverse.py
@@ -4,8 +4,8 @@ num = 1
 
 for i in range(1, row + 1):
     for s in range(row - i): 
-        print(" ", end=" ")
+        print(" ", end=" ") # Print spaces before numbers
     for j in range(i):
         print(num, end=" ")
         num +=1 
-    print()
+    print() # Move to the next line after each row
